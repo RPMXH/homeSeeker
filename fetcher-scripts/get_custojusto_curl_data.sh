314d29b0e5373ae9195ca6ae166d16c4de3a6e84
rpmxh <rafael.piedade.marques@gmail.com>
2025-04-08 00:46:29 +0100

    update docker compose and adapted to accept different articles

--- fetcher-scripts/get_custojusto_curl_data.sh
+++ fetcher-scripts/get_custojusto_curl_data.sh
@@ -1,41 +1,43 @@
-#!/bin/bash
 
 source ./db_handler.sh
 
-cj_curls=$(ls ./curls/custojusto*)
-cj_prefix=$(cat ./curls/cj_prefix.txt)
-cj_suffix=$(cat ./curls/cj_suffix.txt)
-# echo $cj_curls
+get_custojusto_curl_data(){
 
-echo "$cj_curls" | while read -r cj_curl; do
-  #echo $cj_curl
+  local article=$1
 
-  output=$(cat $cj_curl | bash)
+  cj_curls=$(ls ./curls/$article/custojusto*)
+  cj_prefix=$(cat ./curls/custojusto_prefix.txt)
+  cj_suffix=$(cat ./curls/custojusto_suffix.txt)
+  #echo $cj_curls
 
-    echo $output | pup 'a[class^="itemCard_link"]' | while read -r result; do
-      if ! [[ "${result:0:2}" == '<a' ]]; then
-        continue
-      fi
+  echo "$cj_curls" | while read -r cj_curl; do
+    #echo $cj_curl
 
-      href_str=$(awk -F'href' '{print $2}' <<< "$result")
-      href_str_length=${#href_str}
-      href=$(echo ${href_str:2:$((href_str_length-4))})
+    output=$(cat $cj_curl | bash)
 
-      id=${href: -8:8}
+      echo $output | pup 'a[class^="itemCard_link"]' | while read -r result; do
+        if ! [[ "${result:0:2}" == '<a' ]]; then
+          continue
+        fi
 
-      page_html=$(echo "$cj_prefix$href$cj_suffix --silent" | bash)
-      price=$(echo "$page_html" | pup 'span[class^="vi_price"] json{}' | jq '.[1].children[1].text' | tr -d -c '0-9')
+        href_str=$(awk -F'href' '{print $2}' <<< "$result")
+        href_str_length=${#href_str}
+        href=$(echo ${href_str:2:$((href_str_length-4))})
 
-      url=$(echo "https://www.custojusto.pt$href")
+        id=${href: -8:8}
 
-      # Crucially, we use printf %q to properly escape the URL for use in the SQL statement, preventing SQL injection.
-      escaped_url=$(printf %q "$url")
+        page_html=$(echo "$cj_prefix$href$cj_suffix --silent" | bash)
+        price=$(echo "$page_html" | pup 'span[class^="vi_price"] json{}' | jq '.[1].children[1].text' | tr -d -c '0-9')
 
-      upsert_listing "custojusto-$id" $price $escaped_url "$cj_curl"
+        url=$(echo "https://www.custojusto.pt$href")
 
-    sleep 4
+        # Crucially, we use printf %q to properly escape the URL for use in the SQL statement, preventing SQL injection.
+        escaped_url=$(printf %q "$url")
 
-    done
-done
+        upsert_listing "$article-custojusto-$id" $price $escaped_url "$cj_curl"
 
-exit 0
+      sleep 4
+
+      done
+  done
+}

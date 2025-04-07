8c777afb941459961676920b8e77ffe53d97096d
rpmxh <rafael.piedade.marques@gmail.com>
2025-04-07 22:21:34 +0100

    olx refactore to receive other types of articles

--- fetcher-scripts/get_olx_curl_data.sh
+++ fetcher-scripts/get_olx_curl_data.sh
@@ -1,40 +1,71 @@
-#!/bin/bash
 
 source ./db_handler.sh
 
-olx_curls=$(ls ./curls/olx*)
-# echo $olx_curls
+get_olx_home_curl_data(){
 
-loc_prefix="São Mamede De Infesta E Senhora Da Hora"
+  local article=$1
 
-echo "$olx_curls" | while read -r olx_curl; do
-  #echo $olx_curl
+  olx_curls=$(ls ./curls/$article/olx*)
+  #echo $olx_curls
 
-  output=$(cat $olx_curl | bash)
+  loc_prefix="São Mamede De Infesta E Senhora Da Hora"
 
-    #echo "$output" | pup -c 'div[data-cy="l-card"] json{}' | jq '.[1]'
-    echo "$output" | pup 'div[data-cy="l-card"] json{}' | jq -c '.[]' | while read -r result; do
-      id=$(echo "$result" | jq -r '.id')
+  echo "$olx_curls" | while read -r olx_curl; do
+    #echo $olx_curl
 
-      html_result=$(echo "$result" | jq '.children[0].children[0].children[1]')
-      loc=$(echo "$html_result" | jq -r '.children[2].children[0].text')
+    output=$(cat $olx_curl | bash)
 
-      if [[ ! "$loc" =~ ^"$loc_prefix" ]]; then
-        continue
-      fi
+      #echo "$output" | pup -c 'div[data-cy="l-card"] json{}' | jq '.[1]'
+      echo "$output" | pup 'div[data-cy="l-card"] json{}' | jq -c '.[]' | while read -r result; do
+        id=$(echo "$result" | jq -r '.id')
 
-      price=$(echo "$html_result" | jq '.children[0].children[1].text' | tr -d -c '0-9')
-      url=$(echo "$html_result" | jq -r '.children[0].children[0].href')
-      if !  echo "$url" | grep -q "^http"; then
-        continue
-      fi
+        html_result=$(echo "$result" | jq '.children[0].children[0].children[1]')
+        loc=$(echo "$html_result" | jq -r '.children[2].children[0].text')
 
-      # Crucially, we use printf %q to properly escape the URL for use in the SQL statement, preventing SQL injection.
-      escaped_url=$(printf %q "$url")
+        if [[ ! "$loc" =~ ^"$loc_prefix" ]]; then
+          continue
+        fi
 
-      upsert_listing "olx-$id" $price $escaped_url "$olx_curl"
+        price=$(echo "$html_result" | jq '.children[0].children[1].text' | tr -d -c '0-9')
+        url=$(echo "$html_result" | jq -r '.children[0].children[0].href')
 
-    done
-done
+        if !  echo "$url" | grep -q "^http"; then
+          continue
+        fi
 
-exit 0
+        # Crucially, we use printf %q to properly escape the URL for use in the SQL statement, preventing SQL injection.
+        escaped_url=$(printf %q "$url")
+
+        upsert_listing "$article-olx-$id" $price "https://www.olx.pt$escaped_url" "$olx_curl"
+
+      done
+  done
+}
+
+get_olx_curl_data(){
+
+  local article=$1
+
+  olx_curls=$(ls ./curls/$article/olx*)
+  #echo $olx_curls
+
+  echo "$olx_curls" | while read -r olx_curl; do
+    #echo $olx_curl
+
+    output=$(cat $olx_curl | bash)
+
+      #echo "$output" | pup -c 'div[data-cy="l-card"] json{}' | jq '.[1]'
+      echo "$output" | pup 'div[data-cy="l-card"] json{}' | jq -c '.[]' | while read -r result; do
+        id=$(echo "$result" | jq -r '.id')
+        html_result=$(echo "$result" | jq '.children[0].children[0].children[1]')
+        price=$(echo "$html_result" | jq '.children[0].children[1].text' | tr -d -c '0-9')
+        url=$(echo "$html_result" | jq -r '.children[0].children[0].href')
+
+        # Crucially, we use printf %q to properly escape the URL for use in the SQL statement, preventing SQL injection.
+        escaped_url=$(printf %q "$url")
+
+        upsert_listing "$article-olx-$id" $price "https://www.olx.pt$escaped_url" "$olx_curl"
+
+      done
+  done
+}

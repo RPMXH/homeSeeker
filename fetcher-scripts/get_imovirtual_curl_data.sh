c2918f2476ed7b89185ee76a53a58c984d6a3566
rpmxh <rafael.piedade.marques@gmail.com>
2025-04-25 10:41:28 +0100

    fixed id extraction logic for imovirtual

--- fetcher-scripts/get_imovirtual_curl_data.sh
+++ fetcher-scripts/get_imovirtual_curl_data.sh
@@ -2,6 +2,21 @@
 
 source ./db_handler.sh
 
+extract_listing_id() {
+  local hyperlink="$1"
+  local id=""
+
+  # Extract the part after the last '-'
+  # ${hyperlink##*-} removes the longest match of '*-' from the beginning of the string
+  id="${hyperlink##*-}"
+
+  # If the extracted part contains a '?', remove everything from '?' onwards
+  # ${id%%\?*} removes the shortest match of '?*' from the end of the string
+  id="${id%%\?*}"
+
+  echo "$id"
+}
+
 imovirtual_curls=$(ls ./curls/home/imovirtual*)
 # echo $imovirtual_curls
 
@@ -9,11 +24,12 @@ echo "$imovirtual_curls" | while read -r imovirtual_curl; do
   # echo $imovirtual_curl
   output=$(cat $imovirtual_curl | bash)
 
+    #echo $output | pup 'article[data-cy="listing-item"] > section json{}' | jq '.[1]'
     echo $output | pup 'article[data-cy="listing-item"] > section json{}' | jq -c '.[]' | while read -r result; do
 
       href=$(echo $result | jq '.children[1].children[1].href')
 
-      id=${href: -8:7}
+      id=$(extract_listing_id "$href")
       url=$(echo "$href" | jq -r '"https://www.imovirtual.com\(.)"')
       escaped_url=$(printf %q "$url")
       price=$(echo "$result" | jq '.children[1].children[0].children[0].children[0].text' | tr -d -c '0-9')

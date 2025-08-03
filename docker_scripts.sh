08c4e72baef7de71eff9d28e467feb9b36cfad45
rpmxh <rafael.piedade.marques@gmail.com>
2025-08-03 16:17:12 +0100

    added fetcher as a dependency

--- docker_scripts.sh
+++ docker_scripts.sh
@@ -1,3 +1,3 @@
-docker-compose down -v --rmi all #CAREFUL, removes everything, volumes, images
+#docker-compose down -v --rmi all #CAREFUL, removes everything, volumes, images
 
 docker-compose up -d
--- fetcher-scripts/db_handler.sh
+++ fetcher-scripts/db_handler.sh
@@ -2,15 +2,15 @@ upsert_listing () {
   local id=$1
   local price=$2
   local escaped_url=$3
-  local curl=$4
+  local curl_source=$4
 
   # Use mysql -e to execute a SQL command.
-  sql_result_count=$(mysql -h localhost -P 3306 --protocol=tcp listing_database \
-    -e "SELECT * FROM listings WHERE listing_id = '$id';" | wc -l)
+  sql_select_command="SELECT * FROM listings WHERE listing_id = '$id';"
+  sql_result_count=$(execute_mysql_command "$sql_select_command" | wc -l)
 
   if [ $sql_result_count -gt 1 ] ; then
-    mysql -h localhost -P 3306 --protocol=tcp listing_database \
-      -e "UPDATE listings SET price ='$price' WHERE listing_id = '$id';"
+    sql_update_command="UPDATE listings SET price ='$price' WHERE listing_id = '$id';"
+    execute_mysql_command "$sql_update_command"
 
     # Check for MySQL errors.
     if [ $? -ne 0 ]; then
@@ -20,9 +20,9 @@ upsert_listing () {
       echo "Updated record: $escaped_url" #Optional to know it is going as supposed
     fi
   else
-    mysql -h localhost -P 3306 --protocol=tcp listing_database \
-     -e "INSERT INTO listings (listing_id, price, listing_link, source)
-           VALUES ('$id', '$price', '$escaped_url', '$curl');"
+    sql_insert_command="INSERT INTO listings (listing_id, price, listing_link, source)
+           VALUES ('$id', '$price', '$escaped_url', '$curl_source');"
+    execute_mysql_command "$sql_insert_command"
 
     # Check for MySQL errors.
     if [ $? -ne 0 ]; then
@@ -32,6 +32,17 @@ upsert_listing () {
       echo "Inserted record: $escaped_url" #Optional to know it is going as supposed
     fi
   fi
+}
 
+execute_mysql_command() {
+  local sql_command="$1"
 
+  # The -N flag suppresses column headers, -s is for silent mode
+  mysql -h "$MYSQL_HOST" \
+    --port=3306 \
+    --protocol=tcp \
+    "$MYSQL_DATABASE" \
+    --user="$MYSQL_USER" \
+    --password="$MYSQL_PASSWORD" \
+    --execute="$sql_command"
 }
